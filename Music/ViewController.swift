9a32a4dee86290bb5bcd0935b90774f6e1ff38aa
y-nakane <y-nakane@sakura.ad.jp>
2018-11-12 19:51:12 +0900

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Yusuke Nakane on 2018/11/12.
+//  Copyright © 2018 usayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
